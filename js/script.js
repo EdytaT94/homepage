549b541c71dab1dc68724e3e7d20710b20e69226
Edyta Trzebska <edytatrzebska@gmail.com>
2023-03-25 12:55:17 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+console.log("Cześć!");
+
+let button = document.querySelector(".js-header__button");
+let body = document.querySelector(".body");
+let colorSwitch = document.querySelector(".js-header__colorSwitch");
+
+button.addEventListener("click", () => {
+  body.classList.toggle("body--dark");
+
+  colorSwitch.innerText = body.classList.contains("body--dark")
+    ? "białe"
+    : "fioletowe";
+});
